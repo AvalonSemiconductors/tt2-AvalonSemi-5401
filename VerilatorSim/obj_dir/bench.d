3d7674a05ec4db670ef7b01c042411021398a603
Tholin <cutie@tholin.dev>
2024-02-29 10:13:16 +0100

    Fix gitignore

--- VerilatorSim/obj_dir/bench.d
+++
@@ -1,5 +0,0 @@
-bench.o: ../bench.cpp Vtb.h /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_config.h \
- /usr/share/verilator/include/verilated_types.h \
- /usr/share/verilator/include/verilated_funcs.h
